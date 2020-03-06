704d53c2365fcc089df91c80de36333aa41aaae5
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2020-03-06 01:28:48 -0600

    map quest API down error message

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+// ENV
+dotenv.config({ path: "./config/config.env" });
+
+// connect DB
+connectDB();
+const app = express();
+
+// Body Parser
+app.use(express.json());
+
+// Enable Cors
+app.use(cors());
+
+// routes
+app.use("/api/v1/stores", require("./routes/stores"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`server running ${process.env.NODE_ENv} mode on port ${PORT}`)
+);
